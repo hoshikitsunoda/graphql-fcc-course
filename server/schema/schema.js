693f6d806fc766b2bc3c646e2cf60bdba8f7b934
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-22 00:28:58 +0900

    added dummy author data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,12 @@ var books = [
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ]
 
+var authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+]
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
