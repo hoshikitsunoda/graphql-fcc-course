b18061ec5dfe73bcef4c2aecdd236431182a36ba
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-23 00:34:26 +0900

    created mongoose connection to database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const app = express()
+dotenv.config()
+const password = process.env.PASSWORD
+
+mongoose.connect(
+  `mongodb+srv://Hoshki:${password}@graphql-fcc-course-kw6of.mongodb.net/test?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+mongoose.connection.once('open', () => {
+  console.log('connected to database')
+})
 
 app.use(
   '/graphql',
