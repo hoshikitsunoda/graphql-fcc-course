a77c7fbfcfc1e055cd27caa4a997700ef1ea59d2
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-23 01:57:23 +0900

    removed comments

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -21,7 +21,6 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        // return _.find(authors, { id: parent.authorId })
         return Author.findById(parent.authorId)
       },
     },
@@ -37,7 +36,6 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return _.filter(books, { authorId: parent.id })
         return Book.find({ authorId: parent.id })
       },
     },
@@ -51,8 +49,6 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get data from db / other source
-        // return _.find(books, { id: args.id })
         return Book.findById(args.id)
       },
     },
@@ -60,21 +56,18 @@ const RootQuery = new GraphQLObjectType({
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // return _.find(authors, { id: args.id })
         return Author.findById(args.id)
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return books
         return Book.find({})
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        // return authors
         return Author.find({})
       },
     },
