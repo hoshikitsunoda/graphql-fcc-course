0237a70ac19546ecc30b522c462129061c2dcd5f
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-23 23:13:40 +0900

    added cors

--- server/app.js
+++ server/app.js
@@ -3,10 +3,14 @@ const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
 
 const app = express()
 dotenv.config()
 const password = process.env.PASSWORD
+
+// allow cross-origin requests
+app.use(cors())
 
 mongoose.connect(
   `mongodb+srv://Hoshki:${password}@graphql-fcc-course-kw6of.mongodb.net/test?retryWrites=true&w=majority`,
